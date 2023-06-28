540acf462afefeafd2e3db9616e2a6a880431f79
Alexey Tarasov <tarasovion2004@gmail.com>
2023-06-29 00:03:38 +0300

    Initial commit

---
+++ services/sus_plus_plus/src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script>
+	import '../reset.css';
+
+	/**
+	 * @type {HTMLAudioElement}
+	 */
+	let audio;
+
+	function startAudio() {
+		audio.play();
+	}
+</script>
+
+<audio bind:this={audio} src="/audio/theme.mp3" loop />
+
+<svelte:window on:click={startAudio} />
+
+<main>
+	<slot />
+</main>
+
+<style>
+	:global(body) {
+		width: 100vw;
+		height: 100vh;
+
+		background-image: url('/images/stars.png');
+		background-repeat: no-repeat;
+		background-size: cover;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
---
+++ services/sus_plus_plus/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>ඞ</h1>
+
+<style>
+	h1 {
+		color: white;
+	}
+</style>
