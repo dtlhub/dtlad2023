83d61eaf7d3ddef6b3b844c87c150de68caad356
Alexey Tarasov <tarasovion2004@gmail.com>
2023-06-29 11:55:57 +0300

    Add flying amoguses and navigation

--- services/sus_plus_plus/src/routes/+layout.svelte
+++ services/sus_plus_plus/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../reset.css';
+	import FlyingAmogus from '$lib/FlyingAmogus.svelte';
 
 	/**
 	 * @type {HTMLAudioElement}
@@ -11,16 +12,64 @@
 	}
 </script>
 
+<svelte:head>
+	<title>ඞ++</title>
+</svelte:head>
+
 <audio bind:this={audio} src="/audio/theme.mp3" loop />
 
 <svelte:window on:click={startAudio} />
 
+<nav>
+	<ul>
+		<li><a href="/" class="amogus">ඞ++</a></li>
+		<li><a href="/docs">Docs</a></li>
+		<li><a href="/playground">Playground</a></li>
+	</ul>
+	<ul>
+		<li><a href="/auth/login">Login</a></li>
+		<li><a href="/auth/signup">Sign up</a></li>
+	</ul>
+</nav>
+
 <main>
+	{#each { length: 100 } as _}
+		<FlyingAmogus />
+	{/each}
+
 	<slot />
 </main>
 
-<style>
-	:global(body) {
+<style lang="scss">
+	nav {
+		$nav-height: 3em;
+		height: $nav-height;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		background-color: rgba(0, 0, 0, 0.75);
+		margin-bottom: -1 * $nav-height;
+		
+		ul {
+			display: flex;
+			justify-content: space-around;
+			align-items: center;
+
+			li a {
+				margin: 0 0.6em;
+				color: white;
+				text-decoration: none;
+			}
+			.amogus {
+				display: inline;
+				font-size: 2em;
+				color: #c51111;
+			}
+		}
+		position: relative;
+	}
+
+	main {
 		width: 100vw;
 		height: 100vh;
 
@@ -31,5 +80,8 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
+
+		position: relative;
+		z-index: -2;
 	}
 </style>
