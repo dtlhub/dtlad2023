44232826ed0b58d7d4f17cbb906efaff0703f403
Alexey Tarasov <tarasovion2004@gmail.com>
2023-07-19 18:57:34 +0300

    msngr
    * Add msngr
    
    * Add db lock
    
    * Add restart option to docker-compose
    
    * Add msngr to workflow
    
    * Kolhoz
    
    * Kolhoz
    
    * resolved conflict
    
    * Hui
    
    * hiojopa
    
    ---------
    
    Co-authored-by: root <root@DESKTOP-RB4SU4F>
    Co-authored-by: defkit <84741533+jewelofchaos9@users.noreply.github.com>

---
+++ checkers/msngr/checker.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python3
+
+import os
+import sys
+from string import ascii_letters, digits
+from random import choice
+from checklib import *  # noqa: F403
+from Crypto.Util.number import getPrime
+
+
+argv = [c for c in sys.argv]  # https://docs.pwntools.com/en/stable/args.html :)))))))))))
+os.environ['PWNLIB_NOTERM'] = '1'  # https://stackoverflow.com/a/67183309/15078906 :)))))))))))
+from pwn import remote, PwnlibException, context  # IIbIBEH TY/lC b/|RTb  # noqa: E402
+from msngr_lib import CheckMachine  # noqa: E402
+
+context.log_level = 'critical'  # bIbIbIbIbIbIbIbIbIbIbI
+
+
+PORT = 8441
+
+
+def get_random_hello_string(length:int = 15) -> str:
+    alphabet = ascii_letters + digits + r'!#$%&()*+,-./:;<=>?@[]^_`{|}~'
+    return ''.join(choice(alphabet) for _ in range(length))
+
+
+def get_random_secret_string(length: int = 15) -> str:
+    alphabet = ascii_letters + digits
+    return ''.join(choice(alphabet) for _ in range(length))
+
+
+class Checker(BaseChecker):
+    vulns: int = 1
+    timeout: int = 30
+    uses_attack_data: bool = True
+
+    def __init__(self, *args, **kwargs):
+        super(Checker, self).__init__(*args, **kwargs)
+        self.mch = CheckMachine(self)
+
+    def action(self, action, *args, **kwargs):
+        try:
+            super(Checker, self).action(action, *args, **kwargs)
+        except PwnlibException as ex:
+            self.cquit(Status.DOWN, 'Connection error', f'Got pwntools error: {ex}')
+
+    def get_initialized_session(self) -> remote:
+        r = remote(self.host, PORT)
+        r.settimeout(5)
+
+        # This hack works, because `pwn.remote` also has method `.close()`
+        self._sessions.append(r)  # type: ignore
+        # Skip 'welcom to mesfnasensgetR! on SANbKA!!'
+        r.recvline()
+
+        return r
+
+    def check_auth(self):
+        r1 = self.get_initialized_session()
+        username = rnd_username()
+        hello_message = get_random_hello_string()
+        secret_message = get_random_secret_string()
+        token = self.mch.register(r1, username, hello_message, secret_message)
+
+        r2 = self.get_initialized_session()
+        self.mch.login(r2, username, token)
+
+        hello_message_stored = self.mch.get_hello_message(r2)
+        self.assert_eq(hello_message_stored, hello_message, "Hello message differs")
+        secret_message_stored = self.mch.get_secret_message(r2)
+        self.assert_eq(secret_message_stored, secret_message, "Secret message differs")
+
+        expected_registered_users = {username}
+        for _ in range(4):
+            username = rnd_username()
+            hello_message = get_random_hello_string()
+            secret_message = get_random_secret_string()
+            self.mch.register(r2, username, hello_message, secret_message)
+            expected_registered_users.add(username)
+        registered_users = self.mch.list_users(r1)
+
+        for username in expected_registered_users:
+            self.assert_in(username, registered_users, "List of registered users is wrong")
+
+    def check_crypto(self):
+        r = self.get_initialized_session()
+        username = rnd_username()
+        hello_message = get_random_hello_string()
+        secret_message = get_random_secret_string()
+        self.mch.register(r, username, hello_message, secret_message)
+
+        gen = 2
+        mod = getPrime(1024)
+        self.mch.init_key_exchange(r, gen, mod)
+
+        ciphertext, key = self.mch.communicate_dh_feistel(r, username)
+        decrypted_hello_hex = self.mch.decrypt(r, ciphertext, key)
+        decrypted_hello = bytes.fromhex(decrypted_hello_hex)
+        self.assert_eq(decrypted_hello, hello_message.encode(), "Decrypt(encrypt(hello)) != hello")
+
+        # TODO: check commuticate_ask_to_encrypt
+        # TODO: check communicate_ask_for_secret
+
+    def check(self):
+        self.check_auth()
+        self.check_crypto()
+        self.cquit(Status.OK)
+
+    def put(self, flag_id: str, flag: str, vuln: str):
+        r = self.get_initialized_session()
+        username = rnd_username()
+        hello_message = get_random_hello_string()
+        secret_message = flag
+        token = self.mch.register(r, username, hello_message, secret_message)
+        self.cquit(Status.OK, username, f"{username}:{token}")
+
+    def get(self, flag_id: str, flag: str, vuln: str):
+        username, token = flag_id.split(':')
+        r = self.get_initialized_session()
+        self.mch.login(r, username, token)
+        secret_message = self.mch.get_secret_message(r)
+        self.assert_eq(secret_message, flag, "Secret message changed", Status.CORRUPT)
+        self.cquit(Status.OK)
+
+
+if __name__ == '__main__':
+    c = Checker(argv[2])
+
+    try:
+        c.action(argv[1], *argv[3:])
+    except c.get_check_finished_exception():
+        cquit(Status(c.status), c.public, c.private)
---
+++ checkers/msngr/msngr_lib.py
@@ -0,0 +1,109 @@
+import json
+from typing import Tuple, Dict, Any, Set
+from string import hexdigits
+
+from checklib import *
+from pwn import remote
+
+
+class ApiException(Exception):
+    status: str
+    trace: str
+    verbose_description: str
+
+    def __init__(self, status, trace, verbose_description):
+        self.status = status
+        self.trace = trace
+        self.verbose_description = verbose_description
+
+
+class CheckMachine:
+    def __init__(self, checker: BaseChecker):
+        self.c = checker
+
+    def _send_object(self, io: remote, obj: Dict[str, Any]):
+        before_send = json.dumps(obj).encode()
+        io.sendline(before_send)
+
+    def _try_get_from_json(self, line: bytes, field: str, type: type, err_msg: str):
+        try:
+            value = json.loads(line.decode())[field]
+            assert isinstance(value, type)
+            return value
+        except (UnicodeDecodeError, json.JSONDecodeError, KeyError, AssertionError):
+            self.c.cquit(Status.MUMBLE, err_msg, f"Failed to get field {field} on object {line}")
+
+    def login(self, io: remote, username: str, token: str):
+        before_send = json.dumps({"option": "login", "login": username, "token": token}).encode()
+        io.sendline(before_send)
+        return self._try_get_from_json(io.recvline(), "message", str, "Failed to log in")  # type: ignore
+
+    def register(self, io: remote, username: str, hello_message: str, secret_message: str) -> str:
+        self._send_object(
+            io,
+            {
+                "option": "register",
+                "login": username,
+                "hello_message": hello_message,
+                "secret_message": secret_message,
+            },
+        )
+        return self._try_get_from_json(io.recvline(), "token", str, "Failed to register")  # type: ignore
+
+    def get_hello_message(self, io: remote) -> str:
+        self._send_object(io, {"option": "get_hello_message"})
+        return self._try_get_from_json(io.recvline(), "message", str, "Failed to get hello message")  # type: ignore
+
+    def get_secret_message(self, io: remote) -> str:
+        self._send_object(io, {"option": "get_secret_message"})
+        return self._try_get_from_json(io.recvline(), "message", str, "Failed to get secret message")  # type: ignore
+
+    def list_users(self, io: remote) -> Set[str]:
+        self._send_object(io, {"option": "list_users"})
+        users: list = self._try_get_from_json(
+            io.recvline(), "users", list, "Failed to get users"
+        )  # type: ignore
+        self.c.assert_eq(all(isinstance(user, str) for user in users), True, "Failed to get users")
+        return set(users)
+
+    def init_key_exchange(self, io: remote, generator: int, modulus: int) -> str:
+        self._send_object(
+            io,
+            {
+                "option": "init_key_exchange",
+                "generator": hex(generator)[2:],
+                "modulus": hex(modulus)[2:],
+            },
+        )
+        return self._try_get_from_json(io.recvline(), "message", str, "Failed to init key exchange")  # type: ignore
+
+    def communicate_dh_feistel(self, io: remote, friend: str) -> Tuple[str, str]:
+        self._send_object(io, {"option": "communicate_dh_feistel", "friend": friend})
+        response = io.recvline()
+        message: str = self._try_get_from_json(
+            response, "message", str, "Failed to communicate via dh_feistel"
+        )  # type: ignore
+        cipher_key: str = self._try_get_from_json(
+            response, "cipher_key", str, "Failed to communicate via dh_feistel"
+        )  # type: ignore
+        return message, cipher_key
+
+    def communicate_ask_to_encrypt(self, io: remote, data: str, friend: str) -> str:
+        self._send_object(
+            io, {"option": "communicate_ask_to_encrypt", "data": data, "friend": friend}
+        )
+        return self._try_get_from_json(io.recvline(), "message", str, "Failed to ask to encrypt")  # type: ignore
+
+    def communicate_ask_for_secret(self, io: remote, friend: str):
+        self._send_object(io, {"option": "communicate_ask_for_secret", "friend": friend})
+        return self._try_get_from_json(io.recvline(), "message", str, "Failed to ask to secret")  # type: ignore
+
+    def decrypt(self, io: remote, ct: str, key: str) -> str:
+        self._send_object(io, {"option": "decrypt", "ciphertext": ct, "key": key})
+        response: str = self._try_get_from_json(io.recvline(), "message", str, "Failed to decrypt")  # type: ignore
+        self.c.assert_eq(
+            all(char in hexdigits for char in response),
+            True,
+            "Got non-hex result from description",
+        )
+        return response
