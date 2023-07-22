a8f919c7c3f53b0e3bf70f6ef5509b9d56c08f6a
YellowPhil <9093704339@list.ru>
2023-07-22 14:29:06 +0300

    nigger

--- check.py
+++ check.py
@@ -35,11 +35,11 @@ DC_ALLOWED_OPTIONS = DC_REQUIRED_OPTIONS + ['volumes', 'version']
 
 CONTAINER_REQUIRED_OPTIONS = ['restart']
 CONTAINER_ALLOWED_OPTIONS = CONTAINER_REQUIRED_OPTIONS + [
+    'pids_limit', 'mem_limit', 'cpus',
     'build', 'image',
     'ports', 'volumes',
     'environment', 'env_file',
     'depends_on',
-
     'sysctls', 'privileged', 'security_opt',
     'healthcheck', 'expose', 'container_name',
     'links',
